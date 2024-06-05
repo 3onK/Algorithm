63ef8f592c314f85da0d7be62653b2c2749f0453
3onK <66360032+3onK@users.noreply.github.com>
2024-06-05 19:40:42 +0900

    [Bronze V] Title: A+B - 8, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,6 @@
+import sys
+
+c = int(sys.stdin.readline())
+for i in range(c):
+    n1, n2 = map(int, sys.stdin.readline().split())
+    print("Case #", i+1, ": ", n1, " + ", n2, " = ", n1+n2, sep='')
